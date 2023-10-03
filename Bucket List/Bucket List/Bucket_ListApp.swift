bc423ce605839986b3e381d7d14e8391b65794d2
Anna <35114047+AKoneva@users.noreply.github.com>
2023-10-03 22:40:29 +0900

    init

---
+++ Bucket List/Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Анна Перехрест  on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
