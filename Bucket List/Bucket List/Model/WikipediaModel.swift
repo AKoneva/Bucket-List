181cbd11bf82deb22febd10f63199ca5a6ade83f
Anna <35114047+AKoneva@users.noreply.github.com>
2023-10-04 12:16:15 +0900

    Downloading and sorting  data from Wikipedia

---
+++ Bucket List/Bucket List/Model/WikipediaModel.swift
@@ -0,0 +1,33 @@
+//
+//  WikipediaModel.swift
+//  Bucket List
+//
+//  Created by Анна Перехрест  on 2023/10/04.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable, Comparable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+    }
+    
+    static func <(lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
+}
+
+enum LoadingState {
+    case loading, loaded, failed
+}
